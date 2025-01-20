8b1cb951baa888a97534d55eced0e35bcde15698
Rub <menchan512@gmail.com>
2025-01-20 20:54:11 +0900

    Update generate-ssl.sh

--- scripts/generate-ssl.sh
+++ scripts/generate-ssl.sh
@@ -6,7 +6,7 @@ mkdir -p $SSL_DIR
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout $SSL_DIR/private.key \
     -out $SSL_DIR/certificate.crt \
-    -subj "/C=JP/ST=Tokyo/L=Tokyo/O=Server Control Panel/OU=IT/CN=$(hostname)"
+    -subj "/C=JP/ST=Tokyo/L=Tokyo/O=Server Control Panel/OU=IT/CN=localhost"
 
 chmod 600 $SSL_DIR/private.key
 
